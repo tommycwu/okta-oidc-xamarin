62c0432cbf0820aaa394fd3d6b7b13fb17442386
Roger <github@betechie.com>
2019-10-11 14:57:47 -0700

    Update assemblyinfo and project properties to have correct names

--- Okta.Xamarin/Okta.Xamarin.Android/Properties/AssemblyInfo.cs
+++ Okta.Xamarin/Okta.Xamarin.Android/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Okta.Xamarin.Android")]
+[assembly: AssemblyTitle("Okta.Xamarin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Okta")]
-[assembly: AssemblyProduct("Okta.Xamarin.Android")]
+[assembly: AssemblyProduct("Okta.Xamarin")]
 [assembly: AssemblyCopyright("Copyright ©  2019 Okta")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
